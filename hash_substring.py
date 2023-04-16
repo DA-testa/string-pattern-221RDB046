83b17bf20ec480b4e51517bea6eb8220539536d4
Markuss <125598120+221RDB046@users.noreply.github.com>
2023-04-16 19:11:21 +0000

    commit

--- hash_substring.py
+++ hash_substring.py
@@ -11,9 +11,7 @@ def read_input():
         with open(file_name, 'r') as file:
             pattern = file.readline().rstrip()
             text = file.readline().rstrip()
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
+
     return (pattern, text)
 
 def print_occurrences(output):
@@ -21,21 +19,16 @@ def print_occurrences(output):
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    # this function should find the occurances using Rabin Karp alghoritm
     pattern_hash = hash(pattern)
-    text_hash = hash(text[:len(pattern)])
     occurrences = []
 
     for i in range (len(text) - len(pattern) + 1):
         if pattern_hash == hash(text[i:i+len(pattern)]) and pattern == text[i:i+len(pattern)]:
             occurrences.append(i)
-        if i < len(text) - len(pattern):
-            text_hash = hash(text[i+1:i+len(pattern)+1]) + hash(text[i]) - hash(text[i+len(pattern)])
- 
-    return occurrences 
+    return occurrences
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
